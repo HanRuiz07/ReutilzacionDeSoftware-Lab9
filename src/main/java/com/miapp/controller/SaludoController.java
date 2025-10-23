419cb2a1e51646623ad3682df5289e2f4c196abe
Han Kurt Ruiz Espinoza <hanruiz2002@gmail.com>
2025-10-23 17:55:28 -0500

    2do commit

--- src/main/java/com/miapp/controller/SaludoController.java
+++ src/main/java/com/miapp/controller/SaludoController.java
@@ -9,18 +9,15 @@ public class SaludoController {
 
     private final SaludoService saludoService;
 
-    // Inyección de dependencias mediante el constructor
     public SaludoController(SaludoService saludoService) {
         this.saludoService = saludoService;
     }
 
-    // Endpoint: GET /api/saludo/{nombre}
     @GetMapping("/{nombre}")
     public String saludar(@PathVariable String nombre) {
         return saludoService.obtenerSaludo(nombre);
     }
 
-    // Endpoint: GET /api/saludo
     @GetMapping
     public String saludarPorDefecto() {
         return saludoService.obtenerSaludo("visitante");
